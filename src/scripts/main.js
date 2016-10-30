6ac7cd2a4045824e359b62f215b518ba5f3c35ce
in-in <0866222@gmail.com>
2016-10-30 04:23:14 +0300

    add scale font

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,4 +4,11 @@
 //
 // })(jQuery);
 console.log('main');
-// svg4everybody();
+
+function showFontSize() {
+  const foo = document.querySelector('.foo');
+  const fontSize = window.getComputedStyle(foo, null).getPropertyValue('font-size');
+  console.log(fontSize);
+}
+
+window.addEventListener('resize', showFontSize, false);
