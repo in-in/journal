ed6e829db8e887e477e496d34f4a0052d4b04a69
in-in <0866222@gmail.com>
2017-02-17 00:49:19 +0300

    add images for latest posts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -32,56 +32,127 @@ const carouselContent = [
 const latestPosts = [
   {
     position: 'vertical',
-    img: 'post',
+    img: {
+      name: 'post1',
+      widht: [
+        230,
+        465,
+        683,
+        780
+      ]
+    },
     category: 'technology',
     title: 'It was a simple matter to throw off the covers',
     desc: 'But it became difficult after that, especially as he was so exceptionally broad. He would have used his arms and his hands to push himself up'
   },
   {
     position: 'vertical',
-    img: 'post',
+    img: {
+      name: 'post2',
+      widht: [
+        230,
+        351,
+        447,
+        535,
+        611,
+        673,
+        740,
+        780
+      ]
+    },
     category: 'business',
     title: 'The first thing he wanted to do was get the lower part',
     desc: 'So then he tried to get the top part of his body out of the bed first, carefully turning his head to the side. This he managed quite easily, and despite its breadth and its weight'
   },
   {
     position: 'vertical',
-    img: 'post',
+    img: {
+      name: 'post3',
+      widht: [
+        230,
+        395,
+        525,
+        639,
+        743,
+        780
+      ]
+    },
     category: 'travel',
     title: 'At the same time, though, he did not forget to remind himself that calm',
     desc: 'At times like this he would direct his eyes to the window and look out as clearly as he could, but unfortunately, even the other side of the narrow street was enveloped in morning fog and the view had little confidence or cheer to offer him.'
   },
   {
     position: 'vertical',
-    img: 'post',
+    img: {
+      name: 'post4',
+      widht: [
+        230,
+        481,
+        709,
+        780
+      ]
+    },
     category: 'entertainment',
     title: 'It took just as much effort to get back to where he had been earlier',
     desc: 'He told himself once more that it was not possible for him to stay in bed and that the most sensible thing to do would be to get free of it in whatever way he could at whatever sacrifice.'
   },
   {
     position: 'vertical',
-    img: 'post',
+    img: {
+      name: 'post5',
+      widht: [
+        230,
+        467,
+        678,
+        780
+      ]
+    },
     category: 'travel',
     title: 'And he lay there quietly a while longer',
     desc: 'But then he said to himself: "Before it strikes quarter past seven I\'ll definitely have to have got properly out of bed.'
   },
   {
     position: 'vertical',
-    img: 'post',
+    img: {
+      name: 'post6',
+      widht: [
+        230,
+        467,
+        678,
+        780
+      ]
+    },
     category: 'technology',
     title: 'When Gregor was already sticking half way out of the bed',
     desc: 'Two strong people - he had his father and the maid in mind - would have been more than enough; they would only have to push their arms under the dome of his back'
   },
   {
     position: 'vertical',
-    img: 'post',
+    img: {
+      name: 'post7',
+      widht: [
+        230,
+        519,
+        721,
+        780
+      ]
+    },
     category: 'business',
     title: 'After a while he had already moved so far across',
     desc: 'The time was now ten past seven and he would have to make a final decision very soon. Then there was a ring at the door of the flat.'
   },
   {
     position: 'vertical',
-    img: 'post',
+    img: {
+      name: 'post8',
+      widht: [
+        230,
+        450,
+        618,
+        758,
+        780
+      ]
+    },
     category: 'entertainment',
     title: 'For a moment everything remained quiet',
     desc: 'Gregor only needed to hear the visitor\'s first words of greeting and he knew who it was - the chief clerk himself.'
@@ -124,3 +195,51 @@ const page = new Vue({
     }
   }
 });
+
+
+// post1_230
+// post1_465
+// post1_683
+// post1_780
+
+// post2_230
+// post2_351
+// post2_447
+// post2_535
+// post2_611
+// post2_673
+// post2_740
+// post2_780
+
+// post3_230
+// post3_395
+// post3_525
+// post3_639
+// post3_743
+// post3_780
+
+// post4_230
+// post4_481
+// post4_709
+// post4_780
+
+// post5_230
+// post5_467
+// post5_678
+// post5_780
+
+// post6_230
+// post6_478
+// post6_707
+// post6_780
+
+// post7_230
+// post7_519
+// post7_721
+// post7_780
+
+// post8_230
+// post8_450
+// post8_618
+// post8_758
+// post8_780
